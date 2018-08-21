ad99093c15533cd7dc04e9b9ca72dcd2f55ba8cb
Vinaygb <vinaygb665@gmail.com>
2018-08-22 03:25:39 +0530

    1

---
+++ index.js
@@ -0,0 +1,11 @@
+var express =require('express')
+var app= express();
+
+app.get('/',{
+	res.send('papp db api live');
+});
+
+app.listen('80',function(err,res){
+	if(!err) console.log('Success');
+});
+
