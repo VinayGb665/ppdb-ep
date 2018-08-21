69d6b767578fbb9fe7eb332714c580f299a00fc9
Vinaygb <vinaygb665@gmail.com>
2018-08-22 03:55:51 +0530

    1

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express =require('express')
 var app= express();
-
-app.get('/',function(req,res){
-	res.send('papp db api live');
-});
-
 app.listen('3000',function(err,res){
 	if(!err) console.log('Success');
 })
+
+app.get('/',function(req,res){
+	res.send("papp db api live")
+});
+
