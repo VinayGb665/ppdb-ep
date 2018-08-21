f3332cec554b2e98d244381fbbab2ed8d195b9cd
Vinaygb <vinaygb665@gmail.com>
2018-08-22 04:03:39 +0530

    1

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-// var express =require('express')
-// var app= express();
-// app.listen('3000',function(err,res){
-// 	if(!err) console.log('Success');
-// })
-
-// app.get('/',function(req,res){
-// 	res.send("papp db api live")
-// });
-
 const express = require('express')
-// const path = require('path')
- const PORT = process.env.PORT || 5000
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
 express()
   // .use(express.static(path.join(__dirname, 'public')))
   // .set('views', path.join(__dirname, 'views'))
   // .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(5000, () => console.log(`Listening on 5000`))
+   .get('/', (req, res) => res.send('Db API live'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
