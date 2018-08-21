3642d36aa300da31b904bf63170389f4b567ef3b
Vinaygb <vinaygb665@gmail.com>
2018-08-22 03:49:16 +0530

    1

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-var express =require('express')
-var app= express();
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.get('/',function(req,res){
-	res.send('papp db api live');
-});
-
-app.listen('3000',function(err,res){
-	if(!err) console.log('Success');
-});
-
+express()
+  // .use(express.static(path.join(__dirname, 'public')))
+  // .set('views', path.join(__dirname, 'views'))
+  // .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
