be5d2a446d7345b1fb7ec7ab04660b168d20a6db
Vinaygb <vinaygb665@gmail.com>
2018-08-22 03:43:15 +0530

    1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/',function(req,res){
 	res.send('papp db api live');
 });
 
-app.listen('80',function(err,res){
+app.listen('3000',function(err,res){
 	if(!err) console.log('Success');
 });
 
