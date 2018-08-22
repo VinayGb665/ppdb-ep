4dc815f396bf2cff6312f47f7c659bcc015f2a12
Vinaygb <vinaygb665@gmail.com>
2018-08-22 17:52:58 +0530

    1

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const mongoose = require('mongoose');
-const url = process.env.MONGODB_URI;
+
 express()
   // .use(express.static(path.join(__dirname, 'public')))
   // .set('views', path.join(__dirname, 'views'))
   // .set('view engine', 'ejs')
    .get('/', (req, res) => {
-mongoose.connect(url, function (err, db) {
-		  if (err) {
-		    res.send('Unable to connect to the mongoDB server. Error:', err);
-		  } else {
-			res.send('Db API live');
+   		res.send('Live')
 
-		    // do some work here with the database.
-
-		    //Close connection
-		    db.close();
-		  }
-});
-   		}
-   	)
+   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
