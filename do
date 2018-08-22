3842d06dbac7730f66296543e89e55df113940a0
Vinaygb <vinaygb665@gmail.com>
2018-08-22 19:39:39 +0530

    123

---
+++ do
@@ -0,0 +1,3 @@
+git add .
+git commit -m "123"
+git push heroku master
--- models/model.js
+++ models/model.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const url = process.env.MONGODB_URI || "mongodb://heroku_7rl6zx62:9llvcjkd2m40q80u3f2r5dn6hu@ds229732.mlab.com:29732/heroku_7rl6zx62";
+const url =  "mongodb://heroku_7rl6zx62:9llvcjkd2m40q80u3f2r5dn6hu@ds229732.mlab.com:29732/heroku_7rl6zx62";
 mongoose.connect(url)
 
 var formDataSchema = new mongoose.Schema({
+	usn : String,
 	name :String,
 	email :String,
-	usn : String,
 	score_gpa: String
-});
+},{ collection: 'formData' });
 module.exports.formModel = mongoose.model('FormData',formDataSchema);
