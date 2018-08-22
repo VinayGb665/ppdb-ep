e8b7bfcf675d5fdc906e946f897d0fce8ca218a5
Vinaygb <vinaygb665@gmail.com>
2018-08-22 19:47:22 +0530

    123

--- services/services.js
+++ services/services.js
@@ -2,7 +2,7 @@ const models =require('../models/model')
 let formModel =models.formModel;
 let services ={
 	getFormData : (req,res)=>{
-		formModel.find({},function(err,data){
+		formModel.find({},{_id:0},function(err,data){
 			if(!err) res.send(data);
 		});
 	}
