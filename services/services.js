996b2779dbab84f0e8e99d0741c4ed49ff854756
Vinaygb <vinaygb665@gmail.com>
2018-09-30 12:39:16 +0530

    creds-1

--- services/services.js
+++ services/services.js
@@ -1,11 +1,16 @@
 const models =require('../models/model')
 let formModel =models.formModel;
+let stuModel =models.studentModel;
 let services ={
 	getFormData : (req,res)=>{
 		formModel.find({},{_id:0},function(err,data){
 			if(!err) res.send(data);
 		});
+	},
+	signup : (req,res) =>{
+		res.send(req);
 	}
+
 
 }
 module.exports =services;
