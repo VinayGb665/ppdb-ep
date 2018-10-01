0081c08e707078b2eee89bf0175cf73a5032117f
Vinaygb <vinaygb665@gmail.com>
2018-10-01 22:05:57 +0530

    cred-cors

--- index.js
+++ index.js
@@ -6,22 +6,9 @@ var bodyParser = require('body-parser')
 var cors =require('cors')
 
 express()
-.options('/login', function (req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.end();
-});
   // .use(express.static(path.join(__dirname, 'public')))
   // .set('views', path.join(__dirname, 'views'))
   // .set('view engine', 'ejs')
-  .all('*', function(req, res, next) {
-    var origin = req.get('origin'); 
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
    .use( bodyParser.json())
    .use(bodyParser.urlencoded({    
     extended: true
@@ -41,10 +28,11 @@ express()
       services.signup(req,res);
    })
    .post('/login',(req,res) => {
-        res.setHeader("Access-Control-Allow-Origin", '(https?://(?:.+\.)?mywebsite\.com(?::\d{1,5})?)$');
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        res.setHeader("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-			
+
+      res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader("Access-Control-Allow-Headers", "*");
+ 	
         //var result=services.login(req,res);
         res.json({"pass":result})
  })
