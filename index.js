9be72c4a26126a95ff9c095f4aa84e6e2f86cba1
Vinaygb <vinaygb665@gmail.com>
2018-10-01 17:53:24 +0530

    cred-cors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ express()
     extended: true
     }))
    .use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*,null");
+      res.header("Access-Control-Allow-Origin", "*");
       next();
     })
    .get('/', (req, res) => {
