62379e4800d880451ccb616e102f45b8cf4291f0
Vinaygb <vinaygb665@gmail.com>
2018-10-01 18:07:18 +0530

    cred-cors

--- services/services.js
+++ services/services.js
@@ -42,7 +42,7 @@ let services ={
 
 		stuModel.findOne({"usn":data.usn},function(err,results){
 			if(err) res.send(err);
-			res.header("Access-Control-Allow-Origin", "*");
+			//res.header("Access-Control-Allow-Origin", "*");
 			res.send(results.password==hash);
 		});
 		
