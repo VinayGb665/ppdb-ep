c91799f8fb4d49a324135acc445c35450e399091
Vinaygb <vinaygb665@gmail.com>
2018-10-01 18:28:25 +0530

    cred-cors

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ express()
    .use(allowCrossDomain)
    .all('*', function(req, res, next) {
     var origin = req.get('origin'); 
+    
     res.header('Access-Control-Allow-Origin', origin);
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header('Access-Control-Allow-Headers', 'Content-Type');
