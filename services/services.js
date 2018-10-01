a4841389729410ced3d169585b877151e9083957
Vinaygb <vinaygb665@gmail.com>
2018-10-01 22:40:27 +0530

    cred-cors

--- services/services.js
+++ services/services.js
@@ -49,7 +49,7 @@ let services ={
 			//res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 			if(err) res.send(err);
 			
-			if(results!=null) res.send(results);
+			if(results) res.send(results.password==hash);
 			else res.send(results);
 		});
 		
