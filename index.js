db1e5ba897b5b8f3e8ca2474f47375eafae50634
Vinaygb <vinaygb665@gmail.com>
2018-10-01 18:26:51 +0530

    cred-cors

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ express()
   // .set('view engine', 'ejs')
    .use( bodyParser.json()) 
    .use(allowCrossDomain)
+   .all('*', function(req, res, next) {
+    var origin = req.get('origin'); 
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
    .use(bodyParser.urlencoded({    
     extended: true
     }))
