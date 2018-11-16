8e48aa08476b3404ec8fd6a2991fe2f06c21b9f7
Vinaygb <vinaygb665@gmail.com>
2018-11-16 22:27:30 +0530

    Forcing htps

--- index.js
+++ index.js
@@ -26,6 +26,18 @@ express()
     extended: true
     }))
     .use(multipartMiddleware)
+    .use(function(req, res, next) {
+      var schema = req.headers['x-forwarded-proto'];
+    
+      if (schema === 'https') {
+        // Already https; don't do anything special.
+        next();
+      }
+      else {
+        // Redirect to https.
+        res.redirect('https://' + req.headers.host + req.url);
+      }
+    })
    .get('/', (req, res) => {
    		res.sendFile(__dirname+'/test.html');
 
