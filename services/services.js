8fb01984f04605e333f7c423b15e2465ac7ec3f0
Vinaygb <vinaygb665@gmail.com>
2018-11-19 21:57:57 +0530

    Weird

--- services/services.js
+++ services/services.js
@@ -281,7 +281,7 @@ let services ={
 	//	}
 	formrespmodel.aggregate([{ $lookup : { from :'formData', localField:'usn', foreignField:'usn',as:'abs' } },{$project :{ "usn":0 }},{
 		$replaceRoot: { newRoot: { $mergeObjects: [ { $arrayElemAt: [ "$abs", 0 ] }, "$$ROOT" ] } }
-	 },{$match :{"abs.company":req.query.company}},{ $project: { abs: 0 } }],(err,results) => {
+	 },{ $project: { abs: 0 } }],(err,results) => {
 		 if(!err) res.send(results)
 		 else res.send(err);
 	 })
