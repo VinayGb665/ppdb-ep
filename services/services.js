9b858ce038bddb578883e395debf168ac89c4872
Vinaygb <vinaygb665@gmail.com>
2018-11-20 09:02:12 +0530

    ok

--- services/services.js
+++ services/services.js
@@ -280,7 +280,7 @@ let services ={
 	//	}
 	formrespmodel.aggregate([{$match:{"company":req.query.company}},{ $lookup : { from :'formData', localField:'usn', foreignField:'usn',as:'abs' } },{$project :{ "usn":0 }},{
 		$replaceRoot: { newRoot: { $mergeObjects: [ { $arrayElemAt: [ "$abs", 0 ] }, "$$ROOT" ] } }
-	 },{ $project: { abs: 0 ,_id:0,__v:0} }],(err,results) => {
+	 },{ $project: { abs: 0 ,_id:0,__v:0 } }],(err,results) => {
 		 if(!err) {	
 			 jsonexport(results,{rowDelimiter: ','}, (err,csv) => {
 				 if(!err) {
